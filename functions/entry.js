17cd3cbd8f7b85586f24a78a1a11ed7f4cb618f7
klartdean-stack <klartdean@gmail.com>
2026-09-16 14:50:10 +0700

    Sync Android V51 Functions entry exports

--- functions/entry.js
+++ functions/entry.js
@@ -1,5 +1,29 @@
+const secure = require("./secure_index.js");
+const moneyHardening = require("./money_hardening.js");
+const adminMoneyActions = require("./admin_money_actions.js");
+const moneyLedger = require("./money_ledger.js");
+const legacyMoneyBlockers = require("./legacy_money_blockers.js");
+const orderSecurityOverride = require("./order_security_override.js");
+const sellerStatusSecurityOverride = require("./seller_status_security_override.js");
+const orderLifecycleActions = require("./order_lifecycle_actions.js");
+const investmentMoneyActions = require("./investment_money_actions.js");
+const withdrawSecurityOverride = require("./withdraw_security_override.js");
+const investmentCompatOverride = require("./investment_compat_override.js");
+const supportAiFallback = require("./support_ai_fallback.js");
 const refundActions = require("./refund_actions.js");
 
 module.exports = {
+  ...secure,
+  ...moneyHardening,
+  ...adminMoneyActions,
+  ...moneyLedger,
+  ...legacyMoneyBlockers,
+  ...orderSecurityOverride,
+  ...sellerStatusSecurityOverride,
+  ...orderLifecycleActions,
+  ...investmentMoneyActions,
+  ...withdrawSecurityOverride,
+  ...investmentCompatOverride,
+  ...supportAiFallback,
   ...refundActions,
 };
